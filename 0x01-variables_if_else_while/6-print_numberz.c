a39235d90c418ccb22d2aa35ce421a25f419c9fc
eiife <davidilerioluwa11@gmail.com>
2023-02-17 17:38:49 -0800

    fixed whitespaces

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Description : print all single digit number of base 10
- * starting from 0, followed by new line
- * Return : 0
+ * main - Entry point for the program
+ *
+ * Return: 0 if successful, non-zero if an error occurred
  */
+
 int main(void)
 {
-        int c = 0;
+	int c = 0;
 
-        while (c < 10)
-        { 
-                putchar(48+c);
-                c++;
-        }
-        
+	while (c < 10)
+	{
+		putchar(48+c);
+		c++;
+	}
+	
 	putchar('\n');
-        return (0);
+	return (0);
 }       
