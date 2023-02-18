61de0bf358be22bc303b8963fdeb2419d1052487
eiife <davidilerioluwa11@gmail.com>
2023-02-17 17:56:53 -0800

    removed whitespaces

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * main - main block
- * Description : prints all single number of base 10
- * starting from 0, followed by a new line.
- * Return : 0
+ * main - Entry point for the program
+ *
+ * Return: 0 if successful, non-zero if an error occurred
  */
+
 int main(void)
 {
-        int c = 0;
-	 
-       	while (c < 10)
-        { 
-                putchar(48 + c);
+	int c = 0;
+	
+	while (c < 10)
+	{
+		putchar(48 + c);
 		if (c != 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
-                c++;
-        }
-
+		c++;
+	}
