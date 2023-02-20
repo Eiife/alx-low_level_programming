409b93d383ccda18c00e3ae63ab968535017e562
eiife <davidilerioluwa11@gmail.com>
2023-02-20 14:03:17 -0800

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,12 +7,9 @@
  */
 
 int main(void)
-{
-	const char msg[] = "and that piece of art is useful\" - Dora Korpar, "
-				"2015-10-19\n";
-
-	const int len = sizeof(msg) - 1;
-
-	write(2, msg, len);
+{	{
+	const char *msg = "and that piece of art is useful\" - "
+			"Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, msg, 59);
 	return (1);
 }
